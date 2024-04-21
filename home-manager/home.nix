daa94e3a2abcbb1b1629aae63c66a72223ae9deb
Kimuræ <jkgit@nym.hush.com>
2024-04-21 15:30:43 -0400

    Removing helix and bottom as I do not use them anymore. Adding Alacritty terminal.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,23 +9,15 @@
   # Cat w/ syntax highlighting
   programs.bat.enable = true;
 
-  # System Monitor
-  programs.bottom.enable = true;
-
   # Search Tools
   programs.ripgrep.enable = true;
   programs.skim.enable = true;
 
   # Shell 
+  programs.alacritty.enable = true;
   programs.fish.enable = true;
   programs.fish.functions.lk = "rg --files $argv | sk --preview=\"bat {} --color always\"";
   
-  # Editors
-  programs.helix.enable = true;
-  programs.helix.settings.editor.whitespace.render.tab = "all";
-  programs.helix.settings.editor.soft-wrap.enable = true;
-  home.sessionVariables.EDITOR = "hx";
-
   # Multi-plexer
   programs.zellij.enable = true;
   programs.zellij.settings.default_shell = "fish";
